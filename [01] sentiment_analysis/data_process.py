53d12e79451fb29353eb7926fd245315f1a65193
Bricoler <ryumin93@gmail.com>
2018-07-08 14:41:54 +0900

    major update for nlp-tensorflow

--- [01] sentiment_analysis/data_process.py
+++ [01] sentiment_analysis/data_process.py
@@ -33,6 +33,26 @@ def morphs_process(lines):
         tokens.append(token)
     return tokens
 
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int((len(data)-1)/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffled_data = data[shuffle_indices]
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
+            
+
 ##################### use tokenizer #####################
 
 def build_vocab(lines, max_vocab=None):
@@ -298,7 +318,7 @@ def sentence_to_index_pos(lines, vocab, max_length=0):
 ################### use pos_extractor ###################
 
 
-################## use morphs_extractor ##################
+################## use morphs_extractor #################
 
 def build_vocab_morphs(lines, max_vocab=None):
     word_counter = Counter()
@@ -427,24 +447,4 @@ def sentence_to_index_morphs(lines, vocab, max_length=0):
 
     return indexes
 
-################## use morphs_extractor ##################
-
-def batch_iter(data, batch_size, num_epochs, shuffle=True):
-    """
-    Generates a batch iterator for a dataset.
-    """
-    data = np.array(data)
-    data_size = len(data)
-    num_batches_per_epoch = int((len(data)-1)/batch_size) + 1
-    for epoch in range(num_epochs):
-        # Shuffle the data at each epoch
-        if shuffle:
-            shuffle_indices = np.random.permutation(np.arange(data_size))
-            shuffled_data = data[shuffle_indices]
-        else:
-            shuffled_data = data
-        for batch_num in range(num_batches_per_epoch):
-            start_index = batch_num * batch_size
-            end_index = min((batch_num + 1) * batch_size, data_size)
-            yield shuffled_data[start_index:end_index]
-            
+################## use morphs_extractor #################
--- [01] sentiment_analysis/models.py
+++ [01] sentiment_analysis/models.py
@@ -9,9 +9,10 @@ class logistic_regression:
         self._build_net()
         
     def _build_net(self):
-        self.input_x = tf.placeholder(tf.float32, shape=(None, self.vocab_size))
-        self.input_y = tf.placeholder(tf.int32, shape=(None,))
-        Y_one_hot = tf.one_hot(self.input_y, self.n_class)
+        with tf.variable_scope("placeholder"):
+            self.input_x = tf.placeholder(tf.float32, shape=(None, self.vocab_size))
+            self.input_y = tf.placeholder(tf.int32, shape=(None,))
+            Y_one_hot = tf.one_hot(self.input_y, self.n_class)
         
         with tf.variable_scope("output", reuse=tf.AUTO_REUSE):
             W = tf.get_variable('W', dtype=tf.float32,
@@ -174,6 +175,77 @@ class RNN:
     def get_accuracy(self, input_x, input_y):
         return self.sess.run(self.accuracy, feed_dict={self.input_x: input_x, self.input_y: input_y})
     
+    
+class LSTM_onehot:
+
+    def __init__(self, sess, vocab_size, hidden_size=128, n_class=2, lr=1e-2, trainable=True):
+        self.sess = sess
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.n_class = n_class
+        self.lr = lr
+        self.trainable = trainable
+        self._build_net()
+
+    def _build_net(self):
+        with tf.variable_scope("placeholder"):
+            self.input_x = tf.placeholder(tf.int32, (None, None))
+            self.input_y = tf.placeholder(tf.int32, (None,))
+            input_length = tf.reduce_sum(tf.sign(self.input_x), axis=1)
+
+        with tf.variable_scope("onehot_encoding", reuse=tf.AUTO_REUSE):
+            onehot_input_x = tf.one_hot(self.input_x, self.vocab_size)
+
+        with tf.variable_scope("recurrent"):
+            cell = tf.nn.rnn_cell.BasicLSTMCell(self.hidden_size)
+            _, states = tf.nn.dynamic_rnn(cell,
+                                          onehot_input_x,
+                                          input_length,
+                                          dtype=tf.float32)
+
+        with tf.variable_scope("output", reuse=tf.AUTO_REUSE):
+            W = tf.get_variable('W', dtype=tf.float32,
+                                initializer=tf.truncated_normal((self.hidden_size, self.n_class)))
+            b = tf.get_variable('b', dtype=tf.float32,
+                                initializer=tf.constant(0.1, shape=(self.n_class,)))
+            logits = tf.nn.xw_plus_b(states.h, W, b)
+            self.prob = tf.reduce_max(tf.nn.softmax(logits), axis=1)
+            self.prediction = tf.cast(tf.argmax(logits, axis=1), tf.int32)
+
+        with tf.variable_scope("loss"):
+            self.loss = tf.reduce_mean(
+                tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=self.input_y))
+
+        with tf.variable_scope("train", reuse=tf.AUTO_REUSE):
+            global_step = tf.Variable(0, trainable=False)
+            learning_rate = tf.train.exponential_decay(self.lr,
+                                                       global_step,
+                                                       1e+3,
+                                                       0.9,
+                                                       staircase=True)
+            optimizer = tf.train.AdamOptimizer(learning_rate)
+            gvs = optimizer.compute_gradients(self.loss)
+            capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gvs]
+            self.train_op = optimizer.apply_gradients(capped_gvs, global_step=global_step)
+
+        with tf.variable_scope("accuracy"):
+            correct = tf.equal(self.prediction, self.input_y)
+            self.accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+        self.sess.run(tf.global_variables_initializer())
+    
+    def get_loss(self, input_x, input_y):
+        return self.sess.run(self.loss, feed_dict={self.input_x: input_x, self.input_y: input_y})
+   
+    def train(self, input_x, input_y):
+        return self.sess.run([self.loss, self.train_op], feed_dict={self.input_x: input_x, self.input_y: input_y})
+
+    def predict(self, input_x):
+        return self.sess.run((self.prediction, self.prob), feed_dict={self.input_x: input_x})
+
+    def get_accuracy(self, input_x, input_y):
+        return self.sess.run(self.accuracy, feed_dict={self.input_x: input_x, self.input_y: input_y})
+    
 
 class LSTM:
 
@@ -252,76 +324,6 @@ class LSTM:
     def get_accuracy(self, input_x, input_y):
         return self.sess.run(self.accuracy, feed_dict={self.input_x: input_x, self.input_y: input_y})
 
-
-class LSTM_onehot:
-
-    def __init__(self, sess, vocab_size, hidden_size=128, n_class=2, lr=1e-2, trainable=True):
-        self.sess = sess
-        self.vocab_size = vocab_size
-        self.hidden_size = hidden_size
-        self.n_class = n_class
-        self.lr = lr
-        self.trainable = trainable
-        self._build_net()
-
-    def _build_net(self):
-        with tf.variable_scope("placeholder"):
-            self.input_x = tf.placeholder(tf.int32, (None, None))
-            self.input_y = tf.placeholder(tf.int32, (None,))
-            input_length = tf.reduce_sum(tf.sign(self.input_x), axis=1)
-
-        with tf.variable_scope("onehot_encoding", reuse=tf.AUTO_REUSE):
-            onehot_input_x = tf.one_hot(self.input_x, self.vocab_size)
-
-        with tf.variable_scope("recurrent"):
-            cell = tf.nn.rnn_cell.BasicLSTMCell(self.hidden_size)
-            _, states = tf.nn.dynamic_rnn(cell,
-                                          onehot_input_x,
-                                          input_length,
-                                          dtype=tf.float32)
-
-        with tf.variable_scope("output", reuse=tf.AUTO_REUSE):
-            W = tf.get_variable('W', dtype=tf.float32,
-                                initializer=tf.truncated_normal((self.hidden_size, self.n_class)))
-            b = tf.get_variable('b', dtype=tf.float32,
-                                initializer=tf.constant(0.1, shape=(self.n_class,)))
-            logits = tf.nn.xw_plus_b(states.h, W, b)
-            self.prob = tf.reduce_max(tf.nn.softmax(logits), axis=1)
-            self.prediction = tf.cast(tf.argmax(logits, axis=1), tf.int32)
-
-        with tf.variable_scope("loss"):
-            self.loss = tf.reduce_mean(
-                tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=self.input_y))
-
-        with tf.variable_scope("train", reuse=tf.AUTO_REUSE):
-            global_step = tf.Variable(0, trainable=False)
-            learning_rate = tf.train.exponential_decay(self.lr,
-                                                       global_step,
-                                                       1e+3,
-                                                       0.9,
-                                                       staircase=True)
-            optimizer = tf.train.AdamOptimizer(learning_rate)
-            gvs = optimizer.compute_gradients(self.loss)
-            capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gvs]
-            self.train_op = optimizer.apply_gradients(capped_gvs, global_step=global_step)
-
-        with tf.variable_scope("accuracy"):
-            correct = tf.equal(self.prediction, self.input_y)
-            self.accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-
-        self.sess.run(tf.global_variables_initializer())
-    
-    def get_loss(self, input_x, input_y):
-        return self.sess.run(self.loss, feed_dict={self.input_x: input_x, self.input_y: input_y})
-   
-    def train(self, input_x, input_y):
-        return self.sess.run([self.loss, self.train_op], feed_dict={self.input_x: input_x, self.input_y: input_y})
-
-    def predict(self, input_x):
-        return self.sess.run((self.prediction, self.prob), feed_dict={self.input_x: input_x})
-
-    def get_accuracy(self, input_x, input_y):
-        return self.sess.run(self.accuracy, feed_dict={self.input_x: input_x, self.input_y: input_y})
     
 class biLSTM:
 
